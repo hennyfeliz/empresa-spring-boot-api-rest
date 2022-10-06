f97887becc89ef364136fe20e13312d7c9d7b7fe
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2022-10-06 00:15:48 -0400

    adding anothers changes to empleado entity

--- src/main/java/com/empresa/Repository/EmpleadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.empresa.Repository;
-
-import com.empresa.Models.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
-}
--- src/main/java/com/empresa/Repository/EmpresaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.empresa.Repository;
-
-import com.empresa.Models.Empresa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
-}
